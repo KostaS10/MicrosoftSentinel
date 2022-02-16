baf3ea1266932e74082f592360060c1a493aec88
ks <kosta.sotic@gmail.com>
2022-02-16 15:59:19 +0100

    initial sentinel deployment

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,12 +1,25 @@
-resource "azurerm_resource_group" "tfsentinel" {
+resource "azurerm_resource_group" "tfrg" {
   name     = var.resource_group_name
   location = var.location
 }
 
 resource "azurerm_log_analytics_workspace" "tflaw" {
   name                = var.sentinel_law_name
-  location            = azurerm_resource_group.tfsentinel.location
-  resource_group_name = azurerm_resource_group.tfsentinel.name
+  location            = azurerm_resource_group.tfrg.location
+  resource_group_name = azurerm_resource_group.tfrg.name
   sku                 = "PerGB2018"
   retention_in_days   = var.sentinel_law_retention
+}
+
+resource "azurerm_log_analytics_solution" "tfsentinel" {
+  solution_name         = "SecurityInsights"
+  location              = azurerm_resource_group.tfrg.location
+  resource_group_name   = azurerm_resource_group.tfrg.name
+  workspace_resource_id = azurerm_log_analytics_workspace.tflaw.id
+  workspace_name        = azurerm_log_analytics_workspace.tflaw.name
+
+  plan {
+    publisher = "Microsoft"
+    product   = "OMSGallery/SecurityInsights"
+  }
 }
